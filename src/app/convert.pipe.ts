7df87a4753fb254cc609a070b3c53a662842547e
Captain Storm <CaptainStorm21@users.noreply.github.com>
2021-04-30 00:56:00 -0700

    added switch statement for a custom pipe

--- src/app/convert.pipe.ts
+++ src/app/convert.pipe.ts
@@ -5,13 +5,30 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ConvertPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
+  transform(value: any,
+            targetUnits: string): any{
+    // Initial code
+    // transform(value: unknown, ...args: unknown[]): unknown {
     // if no value is entered
     if (!value) {
       return'';
     }
-    console.log(value);
-    return (value * 1.60934);
+    // console.log(value);
+    // console.log(m);
+    // console.log(targetUnits);
+    switch (targetUnits) {
+      case 'km':
+        return value * 1.60934;
+      case 'm':
+        return value * 1.60934 * 1000;
+      case 'cm':
+        return value * 1.60934 * 1000 * 1000;
+      default:
+        return new Error ('Target unit not supported');
+    }
+    // return ( value * 1.60934 );
   }
-
 }
+
+
+// args -  it essentially means take whatever other arguments were provided
