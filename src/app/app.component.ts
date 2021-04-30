af022e973a75d5b72a3dc3d6a880dcf7b6eb8f4d
Captain Storm <CaptainStorm21@users.noreply.github.com>
2021-04-29 23:08:21 -0700

    added Height

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-
   name: string;
+  date: string;
+  amount: number;
+  height: number;
 
+  // Name
   onNameChange(value: string) {
     this.name = value;
+  }
+
+  //Payment date
+  onDateSet(value: string) {
+    this.date = value;
+  }
+
+  //Payment
+  onPaymentAmount(value: number) {
+    this.amount = value;
+  }
+
+  //height
+  onHeight(value: number) {
+    this.height = value;
   }
 }
